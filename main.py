d1e194dc94928287dd2b460c70eb282b38537bfb
Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
2023-09-24 13:39:45 +0530

    Streamlit Frontend Comm #1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import env
 import dataAPI
+from dataAPI import data_fetch
 import streamlit as st
 import pickle
 import numpy as np
@@ -15,4 +16,32 @@ def pred(features):
     else:
         return False
     
-print(pred(dataAPI.data_fetch("al3649")))
+st.title("Fake Account Checker")
+html_temp = """
+<div style="background-color:#025246 ;padding:10px">
+<h2 style="color:white;text-align:center;">Check for Fake Twitter Accounts </h2>
+</div>
+"""
+st.markdown(html_temp, unsafe_allow_html=True)
+
+username = st.text_input("Twitter Username","Type Here")
+safe_html="""  
+    <div style="background-color:#F4D03F;padding:10px >
+    <h2 style="color:white;text-align:center;"> This Account is Real</h2>
+    </div>
+"""
+danger_html="""  
+    <div style="background-color:#F08080;padding:10px >
+    <h2 style="color:black ;text-align:center;"> This Account is Fake</h2>
+    </div>
+"""
+
+if st.button("Predict"):
+    data = data_fetch(username)
+    output=pred(data)
+    if output:
+        st.markdown(safe_html,unsafe_allow_html=True)
+    else:
+        st.markdown(danger_html,unsafe_allow_html=True)
+
+# print(pred(dataAPI.data_fetch("al3649")))
