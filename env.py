1d42c6660416b35f175987da1a598473d3e35747
Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
2023-10-05 21:32:05 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,6 +1,18 @@
 import os
 
-os.environ["consumer_key"] = '1bXNhe3VMM9Kr9pTrM3dQtzNp'
-os.environ["consumer_secret"] = 'jHZqtTf5BufEWhZggD5M2iWFRf9Fa8Ktwzbmh9Zk8RTHcAgZhD'
-os.environ["access_token"] = '1705246643002724352-snAV6y6WKD8os9YjqV2Nc9pF2XKXir'
-os.environ["access_token_secret"] = 'JVDhbZj5Gy1BEhLeAYhluxz1odaGsnZTywO8Lgd5Yr6qa'
+os.environ["consumer_key"] = 'MStZ4Up23VGnPa7fyJul7pPEJ'
+os.environ["consumer_secret"] = 'uAQXM4ZWcOXhE8y3TPckfgyoqSQ2XXJlrBOrtRjMwmVpLQSXNR'
+os.environ["access_token"] = '1705257582904987648-LkEaYyvR0XPBzVor3AfNrwZItLV54x'
+os.environ["access_token_secret"] = '20gGtT2SQyksMd6w7a55lHp0sKBudUZfh7oOP3VNO2rEE'
+
+
+
+# #API KEY: MStZ4Up23VGnPa7fyJul7pPEJ
+# API key Secret: uAQXM4ZWcOXhE8y3TPckfgyoqSQ2XXJlrBOrtRjMwmVpLQSXNR
+
+# Bearer Token: AAAAAAAAAAAAAAAAAAAAABliqAEAAAAAzH7ucdcjkd3L%2FH%2Bd7hyW%2F%2FaZgQ0%3DioqMu2sXolymsA8gQlNTJa2XHrtmPacvfMZZqDKTuZuyaOWftl
+
+# Access Token:  1705257582904987648-LkEaYyvR0XPBzVor3AfNrwZItLV54x
+
+# Acces Token Secret:
+# 20gGtT2SQyksMd6w7a55lHp0sKBudUZfh7oOP3VNO2rEE
