fc8abe842743bcbdd61c25261287b8c11a5f69ee
Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
2023-10-05 21:21:24 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials
-cred = credentials.Certificate("creds.json")
-firebase_admin.initialize_app(cred,
+if not firebase_admin._apps:
+    cred = credentials.Certificate("creds.json")
+    firebase_admin.initialize_app(cred,
                               {
                                   'databaseURL': "https://social-suraksha-sih-default-rtdb.asia-southeast1.firebasedatabase.app/"
                               })
