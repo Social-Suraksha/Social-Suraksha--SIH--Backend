7342ba46f29f01bef15fd72d8013c4209661cdbd
Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
2023-10-08 17:24:54 +0530

    Streamlit Secrets for Firebase Auth

--- firebase.py
+++ firebase.py
@@ -1,7 +1,19 @@
 import firebase_admin
+import streamlit as st
 from firebase_admin import credentials
 if not firebase_admin._apps:
-    cred = credentials.Certificate("creds.json")
+    cred = credentials.Certificate({\
+        "type": "service_account",\
+        "project_id": st.secrets.project_id,\
+        "private_key_id": st.secrets.private_key_id,\
+        "private_key": st.secrets.private_key,\
+        "client_email": st.secrets.client_email,\
+        "client_id": st.secrets.client_id,\
+        "auth_uri": st.secrets.auth_uri,\
+        "token_uri": st.secrets.token_uri,\
+        "auth_provider_x509_cert_url": st.secrets.auth_provider_x509_cert_url,
+        "client_x509_cert_url": st.secrets.client_x509_cert_url,\
+        "universe_domain": "googleapis.com"})
     firebase_admin.initialize_app(cred,
                               {
                                   'databaseURL': "https://social-suraksha-sih-default-rtdb.asia-southeast1.firebasedatabase.app/"
