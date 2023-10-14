0bb663911fb00427374f8437be63e54cff37435a
Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
2023-10-14 11:24:57 +0530

    Code Cleanup, Model Replacement,  Tree Visualisations

--- model.py
+++ model.py
@@ -2,64 +2,76 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score,precision_score,recall_score, log_loss
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import plot_tree
 import pickle
-def train_model():
+import matplotlib.pyplot as plt
+
+def train_model(plot = False, model = False):
   df = pd.read_csv("fusers.csv")
   df1 = pd.read_csv("users.csv")
   df = pd.concat([df1,df], axis=0)
   df.reset_index(drop=True, inplace=True)
-  df = df.loc[:, df.columns != "created_at"]
-  df = df.loc[:, df.columns != "profile_background_image_url"]
-  df = df.loc[:, df.columns != "lang"]
-  df = df.loc[:, df.columns != "url"]
-  df = df.loc[:, df.columns != "profile_text_color"]
-  df = df.loc[:, df.columns != "profile_background_image_url_https"]
-  df = df.loc[:, df.columns != "profile_banner_url"]
-  df = df.loc[:, df.columns != "id"]
-  df = df.loc[:, df.columns != "protected"]
-  df = df.loc[:, df.columns != "verified"]
-  df = df.loc[:, df.columns != "profile_image_url_https"]
-  df.loc[df['dataset'] == "E13", 'dataset'] = 1
-  df.loc[df['dataset'] == "TFP", 'dataset'] = 1
-  df.loc[df['dataset'] == "INT", 'dataset'] = 0
-  df.loc[df['dataset'] == "TWT", 'dataset'] = 0
-  df.loc[df['dataset'] == "FSF", 'dataset'] = 0
-  df['description'].fillna(0, inplace=True)
-  df.loc[df['description'] != 0, 'description'] = 1
-  df.loc[df['default_profile'] != 1, 'default_profile'] = 0
-  df = df.loc[:, df.columns != "time_zone"]
-  df = df.loc[:, df.columns != "location"]
-  df = df.loc[:, df.columns != "profile_use_background_image"]
-  df = df.loc[:, df.columns != "default_profile_image"]
-  df = df.loc[:, df.columns != "profile_image_url"]
-  df = df.loc[:, df.columns != "geo_enabled"]
-  df = df.loc[:, df.columns != "fav_number"]
-  df.loc[df['profile_background_tile'] != 1, 'profile_background_tile'] = 0
-  df = df.loc[:, df.columns != "updated"]
-  df.loc[df['profile_sidebar_border_color'] != "C0DEED", 'profile_sidebar_border_color'] = 0
-  df.loc[df['profile_sidebar_border_color'] == "C0DEED", 'profile_sidebar_border_color'] = 1
-  df.loc[df['profile_sidebar_fill_color'] != "DDEEF6", 'profile_sidebar_fill_color'] = 0
-  df.loc[df['profile_sidebar_fill_color'] == "DDEEF6", 'profile_sidebar_fill_color'] = 1
-  df = df.loc[:, df.columns != "profile_link_color"]
-  df = df.loc[:, df.columns != "profile_background_color"]
-  df = df.loc[:, df.columns != "utc_offset"]
-  df
+  columns_to_drop = [
+      "created_at",
+      "profile_background_image_url",
+      "lang",
+      "url",
+      "profile_text_color",
+      "profile_background_image_url_https",
+      "profile_banner_url",
+      "id",
+      "protected",
+      "verified",
+      "profile_image_url_https",
+      "time_zone",
+      "location",
+      "profile_use_background_image",
+      "default_profile_image",
+      "profile_image_url",
+      "geo_enabled",
+      "fav_number",
+      "profile_link_color",
+      "profile_background_color",
+      "utc_offset",
+      "updated",
+      "name",
+      "screen_name",
+  ]
 
-  df = df.loc[:, df.columns != "name"]
-  df = df.loc[:, df.columns != "screen_name"]
-  df
+  df = df.drop(columns = columns_to_drop)
+  mapping = {'E13': 1, 'TFP': 1, 'INT': 0, 'TWT': 0, 'FSF': 0}
+  df['dataset'] = df['dataset'].replace(mapping)
+  df['description'].fillna(0, inplace = True)
+  df['default_profile'].fillna(0, inplace = True) 
+  df["profile_background_tile"].fillna(0, inplace = True)
+  df['profile_sidebar_border_color'] = np.where(df['profile_sidebar_border_color'] == "C0DEED", 1, 0)
+  df['profile_sidebar_fill_color'] = np.where(df['profile_sidebar_fill_color'] == "DDEEF6", 1, 0)
+  df['description'] = np.where(df['description'] != 0, 1, 0)
 
   X = df.loc[:, df.columns != "dataset"]
   y = df["dataset"]
   y=y.astype('int')
-  df.isnull().values.any()
 
   X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state=42)
 
-
   dectree = DecisionTreeClassifier()
   dectree = dectree.fit(X_train,y_train)
-
+  ranfor = RandomForestClassifier()
+  ranfor = ranfor.fit(X_train, y_train)
+  
+  if plot and model:
+    for tree_idx, estimator in enumerate(ranfor.estimators_):
+      plt.figure(figsize=(20, 11.25))
+      plot_tree(estimator, filled=True, feature_names=X.columns)
+      plt.title(f'Decision Tree {tree_idx}')
+      plt.savefig(f"visualisations/tree_{tree_idx}.pdf", dpi = 2400)
+      print(tree_idx)
+      plt.close()
+  elif plot or model:
+      plt.figure(figsize=(20, 11.25))
+      plot_tree(dectree, filled=True, feature_names=X.columns)
+      plt.title(f'Decision Tree')
+      plt.savefig(f"visualisations/dectree.pdf", dpi = 2400)
+      plt.close()
   pickle.dump(dectree,open('model.pkl','wb'))
-
