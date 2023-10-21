d628eaa00950db426d0adedcf2963a8bc2a85564
Madhav Gupta <114633095+M4dhav@users.noreply.github.com>
2023-10-21 21:01:13 +0530

    remove Dead Code

--- model.py
+++ model.py
@@ -1,11 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import plot_tree
 import pickle
-import matplotlib.pyplot as plt
 from lime import lime_tabular
 
 def train_model(gen = False):
@@ -15,21 +12,7 @@ def train_model(gen = False):
   y = df["dataset"]
   y=y.astype('int')
   fetnames = list(X.columns)
-  
-  # if plot and model:
-  #   for tree_idx, estimator in enumerate(ranfor.estimators_):
-  #     plt.figure(figsize=(20, 11.25))
-  #     plot_tree(estimator, filled=True, feature_names=X.columns)
-  #     plt.title(f'Decision Tree {tree_idx}')
-  #     plt.savefig(f"visualisations/tree_{tree_idx}.pdf", dpi = 2400)
-  #     print(tree_idx)
-  #     plt.close()
-  # elif plot or model:
-  #     plt.figure(figsize=(20, 11.25))
-  #     plot_tree(dectree, filled=True, feature_names=X.columns)
-  #     plt.title(f'Decision Tree')
-  #     plt.savefig(f"visualisations/dectree.pdf", dpi = 2400)
-  #     plt.close()
+
   if gen:
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.3, random_state=42)
     
